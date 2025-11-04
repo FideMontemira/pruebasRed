8b18ac30068158f26c9cb50f19adbd2dc80afd24
Fidel Montemira Orozco <zS22013691@estudiantes.uv.mx>
2025-11-04 16:37:56 -0600

    feat: Documentación en Swagger agregada

---
+++ config/swagger.js
@@ -0,0 +1,28 @@
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API de Usuarios',
+      version: '1.0.0',
+      description: 'Documentación de la API de usuarios con Sequelize',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000/api',
+      },
+    ],
+  },
+  apis: ['./routes/*.js'],
+};
+
+const specs = swaggerJsdoc(options);
+
+function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
+}
+
+module.exports = setupSwagger;
